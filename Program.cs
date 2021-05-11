8f70c51809b67855cfc78f1ecb495e82f820be8b
Andres Sainz <andresmsainz@gmail.com>
2021-05-10 21:49:09 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Static Classes!");
+
+            // Error on creating a new instance of static class
+            //Animal animal = new Animal;
+
+            //Access Directly
+            Animal.GetAge();
+        }
+    }
+
+    static class Animal
+    {
+        static public string name;
+        static public void GetAge()
+        {
+            Console.WriteLine("Age = " + (DateTime.Now.Year - DateTime.Now.AddYears(-2).Year));
+        }
+    }
+}
